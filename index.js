41fa6bd9858268f449fbdacf0f3044bd460fb5bb
AmelioMansour <ameliomansour@oakland.edu>
2022-11-17 11:18:46 -0500

    more changes

--- index.js
+++ index.js
@@ -9,15 +9,13 @@ const router = express.Router();
 
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/src/app.html'));
+    res.render("app");
     //__dirname : It will resolve to your project folder. 
 });
 app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname + '/about.html'));
 });
-router.get('/sitemap', function (req, res) {
-    res.sendFile(path.join(__dirname + '/sitemap.html'));
-});
+
 //add the router 
 //app.use('/', router);
 app.listen(process.env.port || 3000);
