6dfd17c9fcb3d75da729ea4e6c0daa4f6a8b2c68
AmelioMansour <ameliomansour@oakland.edu>
2022-11-17 16:40:36 -0500

    testttaaa

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.get('/about', function (req, res) {
 });
 
 //add the router 
-//app.use('/', router);
+app.use('/', router);
 app.listen(process.env.PORT || 3000);
 console.log('Running at Port 3000');
