7ce971463f65321563d966ba51ec32ad141359ba
amelio <ameliomansour@yahoo.com>
2022-11-16 23:50:32 -0500

    index.js stuff

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const { request } = require("express");
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+//Nav routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
