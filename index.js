e86d7de2286e081fe79266c1c4c14c7fc0524e42
AmelioMansour <ameliomansour@oakland.edu>
2022-11-17 11:04:58 -0500

    another one

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const router = express.Router();
 // Setup essential routes 
 
 
-router.get('/', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/src/app.html'));
     //__dirname : It will resolve to your project folder. 
 });
-router.get('/about', function (req, res) {
+app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname + '/about.html'));
 });
 router.get('/sitemap', function (req, res) {
     res.sendFile(path.join(__dirname + '/sitemap.html'));
 });
 //add the router 
-app.use('/', router);
+//app.use('/', router);
 app.listen(process.env.port || 3000);
 console.log('Running at Port 3000');
