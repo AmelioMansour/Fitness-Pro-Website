21e6471981158a393dc48709320401ba42d87515
DKALAJ1239 <105458376+DKALAJ1239@users.noreply.github.com>
2022-11-29 04:27:04 -0500

    Little bit of Style for Workouts Page

--- src/routes/workouts/+page.svelte
+++ src/routes/workouts/+page.svelte
@@ -20,15 +20,13 @@
     let durationStr = 'Duration: '
     let repsStr = 'Reps: '
     let spaceStr = '\xa0'
-
-
-
+    let newLineStr = '\n'
 
     async function addToList() {
         workoutList =  [...workoutList, newWorkout];
         durationList =  [...durationList, newDuration];
         repsList =  [...repsList, newReps];
-        totalArrayList =  [...totalArrayList, workoutStr + newWorkout + spaceStr+ durationStr + newDuration + spaceStr + repsStr + newReps];
+        totalArrayList =  [...totalArrayList, workoutStr + newWorkout + newLineStr+ durationStr + newDuration + newLineStr + repsStr + newReps];
         finalText = (totalArrayList);
 
         const listForWorkout = {newWorkout};
@@ -59,27 +57,29 @@ async function removeFromList(index) {
 <Nav active="workouts" />
 
 
-<div class="container text-center py-3">
+<div class="container text-center py-10">
 	<div class="entry-field pb-3">
 		<form action="POST" onsubmit="return false">
-			<input id="inputField" bind:value={newWorkout} type="text" placeholder="Enter a Workout {enterNewWorkout}" class="rounded">
+			<input id="inputFieldMain" bind:value={newWorkout} type="text" placeholder="Enter a Workout {enterNewWorkout}" class="rounded">
+        <br>
             <input id="inputField" bind:value={newDuration} type="text" placeholder="Enter a Duration {enterNewDuration}" class="rounded">
             <input id="inputField" bind:value={newReps} type="text" placeholder="Enter amount of Reps {enterNewReps}" class="rounded">
-
+        <br>
 			<button on:click={addToList} id="addButton">Add</button>
 		</form>
 	</div>
 
-{#each finalText as itemC, index}
-<p class="text-center listItemC">{itemC}<span><button id="removeItem" on:click={() => {removeFromList(index)}}> <img src="/images/X.png" alt="Remove Item" width="15"> </button> <button id="removeItem" on:click={() => {removeFromList(index)}}> <img src="/images/check-removebg-preview.png" alt="Move Item" width="15"> </button> </span></p>
+{#each finalText as allItems, index}
+<p class="text-center listItemC">{allItems}<span><button id="removeItem" on:click={() => {removeFromList(index)}}> <img src="/images/X.png" alt="Remove Item" width="15"> </button> <button id="removeItem" on:click={() => {removeFromList(index)}}> <img src="/images/check-removebg-preview.png" alt="Move Item" width="15"> </button> </span></p>
 {/each}
 </div>
 
 
 <style>
     #inputField{
-		width: 33%;
-		height: 8vh;
+        float: bottom;
+		width: 30%;
+		height: 7vh;
 		border: 2px solid #3F9FFE;
 		border-color: #3F9FFE;
 	}
@@ -90,9 +90,32 @@ async function removeFromList(index) {
 		color:transparent;
 		transition: .2s;
 	}
-	button, button:active, button:focus {
-		outline: 2px;
-		border: none;
+    #inputFieldMain{
+		width: 60.33%;
+		height: 7vh;
+		border: 2px solid #3F9FFE;
+		border-color: #3F9FFE;
+	}
+	#inputFieldMain:focus{
+		outline: 2px solid #3F9FFE;
+	}
+	#inputFieldMain:focus::placeholder{
+		color:transparent;
+		transition: .2s;
+	}
+	#addButton{
+		margin-top: 20px;
+		border-radius: 10px;
+		color: #0080ff;
+		border: 5px solid #0080ff;
+		width: 15%;
+	}
+	#removeItem{
+		margin-top: 1px;
+		border-radius: 10px;
+		color: #0080ff;
+		border: 4px solid #0080ff;
+		width: 4%;
 	}
 	@media only screen and (max-width: 900px){
 		#inputField{
