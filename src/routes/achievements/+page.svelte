a84b699ff0210204288c551e8efe9dc71aa1e305
amelio <ameliomansour@yahoo.com>
2022-11-30 22:48:05 -0500

    progress on pull achievements

--- src/routes/achievements/+page.svelte
+++ src/routes/achievements/+page.svelte
@@ -1,24 +1,39 @@
-<!-- <script>
+<script>
 	import { name } from 'body-parser';
 	import { text } from 'svelte/internal';
 	import Nav from '../nav.svelte';
-    import List from 'achievements';
+	// import List from 'achievements';
+	let passing = '';
+	async function PullAchievments() {
+		try {
+			console.log('Called!');
+
+			await fetch('/api/addach', {
+				method: 'POST'
+			})
+				.then((Response) => Response.text())
+				.then((data) => {
+					passing = data;
+				});
+		} catch (err) {
+			alert(err);
+			//text = '';
+		}
+		if (result != '') {
+			console.log('goodjob');
+		}
+		console.log(passing);
+	}
 </script>
 
-<List result='achieve'></List> 
+<!-- <List result='achieve'></List>  -->
 
-<Nav active = "achievements"/>
+<Nav active="achievements" />
 
 <div class="container text-center py-3">
 	<div class="entry-field pb-3">
-		<form action="POST" onsubmit="return false">
-			<input
-				type="text"
-				placeholder="Enter new {result}"
-				class="rounded"
-			/>
-			<button on:click={result} id="addButton">Add</button>
-		</form>
+		
+			<button on:click={PullAchievments} id="addButton">Load Achievements</button>
+		
+	</div>
 </div>
-</div> -->
-
